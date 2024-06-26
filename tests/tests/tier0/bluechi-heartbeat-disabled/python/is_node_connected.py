6079ee2a001a84ffe0c92a4f2098d642bf9bfae8
Joonyoung Shim <joonyoung.shim@42dot.ai>
2024-06-26 15:14:25 +0900

    fix2
    Signed-off-by: Joonyoung Shim <joonyoung.shim@42dot.ai>

--- tests/tests/tier0/bluechi-heartbeat-disabled/python/is_node_connected.py
+++ tests/tests/tier0/bluechi-heartbeat-disabled/python/is_node_connected.py
@@ -6,23 +6,27 @@
 import time
 import unittest
 
+from dasbus.typing import Variant
+
 from bluechi.api import Node
 
 
 class TestNodeIsConnected(unittest.TestCase):
     def test_node_is_connected(self):
+
+        def on_state_change(state: Variant):
+            assert False
+
         n = Node("node-foo")
-        print(f"n.status: {n.status}")
         assert n.status == "online"
 
+        n.on_status_changed(on_state_change)
         timestamp = n.last_seen_timestamp
-        print(f"timestamp: {timestamp}")
 
         # verify that the node remains connected and LastSeenTimespamp is not
         # updated after more than NodeHeartbeatThreshold seconds have elapsed
         time.sleep(10)
-        print(f"n.last_seen_timestamp: {n.last_seen_timestamp}")
-        print(f"n.status: {n.status}")
+
         assert n.status == "online"
         assert n.last_seen_timestamp == timestamp
 
--- tests/tests/tier0/bluechi-heartbeat-node-disconnected/python/is_node_disconnected.py
+++ tests/tests/tier0/bluechi-heartbeat-node-disconnected/python/is_node_disconnected.py
@@ -15,48 +15,28 @@ from bluechi.api import Node
 class TestNodeIsDisconnected(unittest.TestCase):
 
     node_state = None
-    curr_state = None
-    curr_last_seen_timestamp = None
+    timestamp = None
 
     def test_node_is_disconnected(self):
-        start_time = time.time()
-
         loop = EventLoop()
 
         def on_state_change(state: Variant):
+            self.timestamp = round(time.time() * 1000000)
             self.node_state = state.get_string()
-
-            n = Node("node-foo")
-            self.curr_state = n.status
-            self.curr_last_seen_timestamp = n.last_seen_timestamp
-
             loop.quit()
 
         n = Node("node-foo")
-        n.on_status_changed(on_state_change)
-        print(f"n.status: {n.status}")
         assert n.status == "online"
 
+        n.on_status_changed(on_state_change)
         timestamp = n.last_seen_timestamp
-        print(f"timestamp: {timestamp}")
 
         loop.run()
 
-        end_time = time.time
-        print(f"start_time: {start_time}")
-        print(f"end_time: {end_time}")
-
-        # verify that the node is disconnected and LastSeenTimespamp is not
-        # updated after more than NodeHeartbeatThreshold seconds have elapsed
-        print(f"n.last_seen_timestamp: {n.last_seen_timestamp}")
-        print(f"n.status: {n.status}")
-        print(f"self.node_state: {self.node_state}")
-        print(f"self.curr_state: {self.curr_state}")
-        print(f"self.curr_last_seen_timestamp: {self.curr_last_seen_timestamp}")
+        # verify that the node is disconnected after more than
+        # NodeHeartbeatThreshold seconds have elapsed
+        assert self.timestamp - timestamp > 6000000
         assert self.node_state == "offline"
-        assert end_time - timestamp > 6000000
-        assert self.curr_state == "offline"
-        assert self.node_last_seen_timestamp == timestamp
 
 
 if __name__ == "__main__":
